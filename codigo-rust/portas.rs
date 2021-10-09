ad2f5097c916a6a3fd4349c2071b2ea305b5761e
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-10-08 23:05:29 -0400

    code in Rust

---
+++ codigo-rust/portas.rs
@@ -0,0 +1,39 @@
+// Curso: Engenharia de Software
+// Disciplina: Sistemas Embarcados
+// Professor: Vandermi Joao da Silva
+// Autor: Gabriel dos Santo Tavares
+// Data: 08/10/2021
+
+// Codifique o circuito desenvolvido na
+// questão 1 em linguagem de programação Rust.
+
+fn main() {
+    // valores
+    //  a	b	x   !x
+    //  0	0	1    0
+    //  0	1	0    1
+    //  1	0	0    1
+    //  1	1	0    1
+
+    //testando na porta OR com valores A  e B
+    //basta trocar os valores que corresponde a tabela
+    println!("!({} or {}) = {}\n",0,1,processamento(0,1,5)); //parametros do processamento
+                               // a,b             //a,b,op
+}
+
+fn processamento(a:i32,b:i32,op: i32) -> i32{
+  
+    if op == 1{//PORTA NOT
+        !(!a)
+    }else if op == 2{ //PORTA NAND 
+        !(!(a&b))
+    }else if op == 3{//PORTA NOR
+        !(!(a|b))
+    }else if op == 4{//PORTA AND
+        !(a&b)+2
+    }else if op == 5{//PORTA OR
+        !(a|b)+2
+    }else{
+        -1
+    }
+}
